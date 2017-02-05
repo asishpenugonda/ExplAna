a187576bf8a2fb37a181a6a7341e26cf252a7cf8
Asish <asish.asish12@gmail.com>
2017-02-05 12:32:16 -0500

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+setwd("./ExplAna/Week1")
+data<- "./HPC.txt"
+HPC<- read.table(data, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+HPC.date<- HPC[HPC$Date %in% c("1/2/2007", "2/2/2007"),]
+globalActivePower<- as.numeric(HPC.date$Global_active_power)
+png("plot1.png")
+hist(globalActivePower, col="red", main="Global Active Power (Kilowatts)")
+dev.off()

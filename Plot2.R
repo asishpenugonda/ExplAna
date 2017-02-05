c7a227780c67bdf3c039a24c7c289c5e32205e58
Asish <asish.asish12@gmail.com>
2017-02-05 12:48:21 -0500

    Update Plot2.R
    Updated file name to Plot2.png from plot2.png

--- Plot2.R
+++ Plot2.R
@@ -3,6 +3,6 @@ HPC<- read.table(data, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
 HPC.date<- HPC[HPC$Date %in% c("1/2/2007", "2/2/2007"),]
 globalActivePower<- as.numeric(HPC.date$Global_active_power)
 HPC.datatime <- strptime(paste(HPC.date$Date, HPC.date$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-png("plot2.png")
+png("Plot2.png")
 plot(HPC.datatime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()

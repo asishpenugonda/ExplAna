97058d8e75972f3f59e17f50ab5741987d92aa71
Asish <asish.asish12@gmail.com>
2017-02-05 12:36:05 -0500

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,8 @@
+data<- "./HPC.txt"
+HPC<- read.table(data, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+HPC.date<- HPC[HPC$Date %in% c("1/2/2007", "2/2/2007"),]
+globalActivePower<- as.numeric(HPC.date$Global_active_power)
+HPC.datatime <- strptime(paste(HPC.date$Date, HPC.date$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot2.png")
+plot(HPC.datatime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()

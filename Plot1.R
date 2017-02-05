b2ba87574acdb1caf9df39b131bfb6532aa9279e
Asish <asish.asish12@gmail.com>
2017-02-05 12:45:26 -0500

    Update Plot1.R
    Changing the file name to Plot1.png from plot.png

--- Plot1.R
+++ Plot1.R
@@ -3,6 +3,6 @@ data<- "./HPC.txt"
 HPC<- read.table(data, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
 HPC.date<- HPC[HPC$Date %in% c("1/2/2007", "2/2/2007"),]
 globalActivePower<- as.numeric(HPC.date$Global_active_power)
-png("plot1.png")
+png("Plot1.png")
 hist(globalActivePower, col="red", main="Global Active Power (Kilowatts)")
 dev.off()
